3cbf651af5f917ba94eceae20447ee4ace1f0fbe
Rich Seviora <richard.seviora@gmail.com>
2017-05-27 11:48:53 -0700

    Now Checks for Winner

--- dist/bundle.js
+++ dist/bundle.js
@@ -162,8 +162,34 @@ var Board = (function (_super) {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
     };
+    Board.prototype.calculateWinner = function (squares) {
+        var lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (var i = 0; i < lines.length; i++) {
+            var _a = lines[i], a = _a[0], b = _a[1], c = _a[2];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
     Board.prototype.render = function () {
-        var status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        var winner = this.calculateWinner(this.state.squares);
+        var status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (React.createElement("div", null,
             React.createElement("div", { className: "status" }, status),
             React.createElement("div", { className: "board-row" },
